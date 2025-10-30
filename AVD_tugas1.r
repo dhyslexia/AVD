65cbb04b8de81ca38a75d19a48afe35fab84da34
‮sgniD ot duP <dhyslexia@gmail.com>
2025-10-31 05:19:38 +0700

    sec commit

--- AVD_tugas1.r
+++ AVD_tugas1.r
@@ -2,9 +2,9 @@
 kecepatan <- c(4,4,7,7,8,8,9,10,10,11,11,12,12,12,13,13,13,13,114,14,14,14,15,15,15,16,16,17,17,17,17,17,18,18,18,19,19,20,20,20,20,20,21,22,23,24,24,24,25,25)
 jarak <- c(2,10,4,20,17,13,18,28,33,18,26,12,24,22,28,24,32,34,43,24,30,58,80,20,24,55,35,40,30,44,50,46,53,70,80,36,46,68,34,48,50,56,60,64,56,72,90,92,110,85)
 
-kecepatan_rata = mean(kecepatan)
-jarak_rata = mean(jarak)
-sd_jarak = sd(jarak)
+kecepatan_rata <- mean(kecepatan)
+jarak_rata <- mean(jarak)
+sd_jarak <- sd(jarak)
 print(paste("kecepatan rata-rata = ", kecepatan_rata))
 print(paste("jarak rata-rata = ", jarak_rata))
 print(paste("sd jarak = ", sd_jarak))
